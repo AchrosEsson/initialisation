e5c220434743bb740d702142012b0629efeafea3
AchrosEsson <47488896+AchrosEsson@users.noreply.github.com>
2021-03-02 23:11:17 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
+OPTIONS=$(getent passwd {1000..60000} | cut -d: -f1) 
+
+echo $'\n'
 PS3="Bitte wähle deinen Benutzer aus: "
-select username in "(getent passwd {1000..60000} | cut -d: -f1)"
-do 
-    echo "Gewähler Benutzer: $username"
+
+
+select username in $OPTIONS
+
+do
+        echo $'\n'
+        echo "Gewähler Benutzer: $username"
+        echo $'\n'
+
+        break
 done
+
+sleep 2
+
