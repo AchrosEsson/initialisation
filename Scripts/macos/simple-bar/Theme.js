d9fee436495e6f0674ea27422891e9cecdadd3da
AchrosEsson <47488896+AchrosEsson@users.noreply.github.com>
2021-03-09 17:43:49 +0100

    Add files via upload

---
+++ Scripts/macos/simple-bar/Theme.js
@@ -0,0 +1,24 @@
+// uncomment this, and comment the rest of the lines to use pywal colors.
+// export { Theme } from './ThemePywal.js'
+
+export const Theme = {
+  main: '#363852',
+  mainAlt: '#98A8C5',
+  minor: '#39465E',
+  accent: '#FFD484',
+  red: '#E78482',
+  green: '#8FC8BB',
+  yellow: '#FFD484',
+  orange: '#FFB374',
+  blue: '#6DB3CE',
+  magenta: '#AD82CB',
+  cyan: '#7EDDDE',
+  background: '#cc363852',
+  lightGrey: 'rgba(0, 0, 0, 0.05)',
+  font: 'Hack Mono, monospace',
+  lightShadow: '0 0px 10px rgba(0, 0, 0, 0.40)',
+  mediumShadow: '0 8px 30px rgba(0, 0, 0, 0.24)',
+  largeShadow: '0 30px 60px rgba(0, 0, 0, 0.24)',
+  easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  workspace: '#a363852'
+}
